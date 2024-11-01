e5217fbd4ca3296cf4efda476e3168b7e09a5f04
nickjaudylol <nicolasjaudy99@gmail.com>
2024-11-01 16:07:47 -0400

    tipos de dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//tipos de dados primitivos 
+//Numeros inteiros
+short ano = 2025;
+int contador = 10;
+long populacaoMundial = 7800000000;
+
+//Numeros de ponto flutuante
+float temperatura = 43.6f;
+double precoProduto = 199.90;
+
+//tipo caracter
+char inicial = "W";
+
+//tipos logicos
+bool estaLogado = true;
+bool ativo = false;
+
+//
