9195d066ce747171e928addacb9bb009b1248fb9
nickjaudylol <nicolasjaudy99@gmail.com>
2024-11-04 17:20:45 -0400

    adicionado metodo parse

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -4,6 +4,18 @@ string nome = "joão";
 string saudacao = "ola," + nome + "!Bem-vindo.";
 Console.WriteLine(saudacao);
 
-string nome = "joao";
-string saudacao = $"Ola,{nome}! Bem-vindo.";
-Console.Writeline(saudacao);
+string nome = "Nicolas";
+string saudacao1 = $"Ola,{nome}! Bem-vindo.";
+Console.Writeline(saudacao1);
+
+int numero = 5;
+Console.WriteLine(numero.gettype());
+
+Console.WriteLine(typeof(int));
+
+string valorTexto ="100";
+Console.Writeline("A variavel valorTexto e do tipo:" + valorTexto.GetType()));
+
+int mumero = int.parse(valorTexto);
+Console.WriteLine(numero);
+Console.WriteLine("A variavel numero parse e do tipo:"+ numero.GetType()))
