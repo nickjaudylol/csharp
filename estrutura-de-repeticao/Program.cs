d0e0dcb0be75fd52be43115d5a1eee4fdbba3919
nickjaudylol <nicolasjaudy99@gmail.com>
2024-11-11 14:47:08 -0400

    adicionado estrutura de repetição do-Whille

--- estrutura-de-repeticao/Program.cs
+++ estrutura-de-repeticao/Program.cs
@@ -1,11 +1,19 @@
-﻿random numeroAleatorio = new random();
-int numero = numeroAleatorio.Next(1,101);
-int contador = 0;
-Console.WriteLine($"Numero Alvo: {numero}");
-//Loop que incrementa o contador até ele atingir o numero alvo
-while (contador != numero)
+﻿// random numeroAleatorio = new random();
+// int numero = numeroAleatorio.Next(1,101);
+// int contador = 0;
+// Console.WriteLine($"Numero Alvo: {numero}");
+// //Loop que incrementa o contador até ele atingir o numero alvo
+// while (contador != numero)
+// {
+//     Console.WriteLine($"Contador: {contador}.");
+//     contador++;
+// }
+// Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
+
+int numero = 0;
+
+do 
 {
-    Console.WriteLine($"Contador: {contador}");
-    contador++;
-}
-Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
+    Console.WriteLine($"Numero: {numero}");
+    numero++;
+} while (numero < 10);
