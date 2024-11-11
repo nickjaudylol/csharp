f6fbcd6c6132e905e88776a64b7d3fc3beccf680
nickjaudylol <nicolasjaudy99@gmail.com>
2024-11-11 14:36:41 -0400

    adicionado estruturas de repetição while

--- estrutura-de-repeticao/Program.cs
+++ estrutura-de-repeticao/Program.cs
@@ -1,1 +1,11 @@
-﻿Random numeroAleatorio
+﻿random numeroAleatorio = new random();
+int numero = numeroAleatorio.Next(1,101);
+int contador = 0;
+Console.WriteLine($"Numero Alvo: {numero}");
+//Loop que incrementa o contador até ele atingir o numero alvo
+while (contador != numero)
+{
+    Console.WriteLine($"Contador: {contador}");
+    contador++;
+}
+Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
